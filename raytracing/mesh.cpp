2c46cbaeaead45f70603dd14b83dbf2fac88c631
Zoheth <z0heth@outlook.com>
2024-02-26 19:42:23 +0800

    merge

--- raytracing/mesh.cpp
+++ raytracing/mesh.cpp
@@ -2,7 +2,7 @@
 
 namespace rt
 {
-Mesh::Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned> &indices, std::shared_ptr<Material> material):
-	vertices(vertices), indices(indices), material(std::move(material))
+Mesh::Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned> &indices, std::shared_ptr<Material> material) :
+    vertices(vertices), indices(indices), material(std::move(material))
 {}
-}
+}        // namespace rt
--- raytracing/scene.h
+++ raytracing/scene.h
@@ -2,40 +2,16 @@
 
 #include <string>
 #include <vector>
-<<<<<<< HEAD
-
-#include <glm/glm.hpp>
-
-struct Vertex
-{
-	glm::vec3 position;
-	glm::vec3 normal;
-	glm::vec2 texcoord;
-};
-
-struct Mesh
-{
-	std::vector<Vertex>       vertices;
-	std::vector<unsigned int> indices;
-};
-=======
 #include <assimp/scene.h>
 
 #include "mesh.h"
 #include "material.h"
->>>>>>> 83cf8f6c757898865dae92f0dc50a6e4d70a8e8b
 
 namespace rt
 {
 class Scene
 {
 public:
-<<<<<<< HEAD
-	void parse_file(std::string filename);
-
-private:
-	std::vector<Mesh> meshes_;
-=======
 	void parse_file(const std::string &filename);
 
 private:
@@ -45,6 +21,6 @@ private:
 private:
 	std::vector<std::shared_ptr<Mesh>> meshes_;
 	MaterialLibrary material_library_;
->>>>>>> 83cf8f6c757898865dae92f0dc50a6e4d70a8e8b
+
 };
 }
